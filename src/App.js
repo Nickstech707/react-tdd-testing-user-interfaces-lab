d4daafc3de68faca650a95a8fc03900327a3821a
Nicholas Muriithi <nickstech707@gmail.com>
2024-05-05 20:34:45 +0300

    Finds elements using accessible queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div>Your code here</div>;
-}
+import React from "react";
+import "./App.js";
 
+function App() {
+  return (
+    <main>
+      <h1>Hi, I'm Nicholas Muriithi</h1>
+      <img
+        src="../public/mario-luigi-yoschi-figures-163036.jpeg"
+        alt="My profile pic"
+      />
+      <h2>About Me</h2>
+      <p>I'm a full stack developer</p>
+      <div>
+        <a href="https://github.com/Nickstech707">GitHub</a>
+        <a href="https://linkedin.com/NicholasMuriithi">LinkedIn</a>
+      </div>
+    </main>
+  );
+}
 export default App;
